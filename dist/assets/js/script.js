4ce846b243b6e853252f6778d662a18afe559682
Marina <shunkina9292@mail.ru>
2022-07-29 08:34:39 +0300

    grid

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -1,7 +1,18 @@
-function test(a, b) {
-    return a + b
-}
+// function test(a, b) {
+//     return a + b
+// }
 
+// $(document).ready(function () {
+//     $(".slider").slick({
+//       infinite: true,
+//       dots: true,
+//       slidesToShow: 1,
+//       slidesToScroll: 1,
+//       autoplay: true,
+//     });
+//   });
+  
+ 
 
 
 const button = document.querySelector(".burger");
--- src/assets/js/lib.js
+++ src/assets/js/lib.js
@@ -1,3 +1,15 @@
-function test(a, b) {
-    return a + b
-}
+// function test(a, b) {
+//     return a + b
+// }
+
+// $(document).ready(function () {
+//     $(".slider").slick({
+//       infinite: true,
+//       dots: true,
+//       slidesToShow: 1,
+//       slidesToScroll: 1,
+//       autoplay: true,
+//     });
+//   });
+  
+ 
