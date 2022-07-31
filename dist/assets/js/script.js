4e40ea02d23c85085da520eeefd653141428c329
Marina <shunkina9292@mail.ru>
2022-07-31 12:01:09 +0300

    scroll js

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -11,6 +11,22 @@ button.addEventListener("click", function () {
   menu.classList.toggle("active");
 });
 
+menu.addEventListener("click", function () {
+  menu.classList.toggle("active");
+});
+
+
+let anchors = document.querySelectorAll('a[href*="#"]');
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function (event) {
+    event.preventDefault();
+    let blockID = anchor.getAttribute("href");
+    document.querySelector(blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+}
 
 const line = document.querySelector(".questions__wrap");
 let details = document.querySelector(".questions__summary");
